8151c5457421dcd559a4adeabf33cbfa2db61391
Krish Koria <krishkoria2004@gmail.com>
2025-02-26 08:32:11 +0000

    Add HTML welcome page and health check endpoint

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "net/http"
 
+func readinessHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8");
+	w.WriteHeader(http.StatusOK);
+	w.Write([]byte("OK"));
+}
+
 func main()  {
 	mux := http.NewServeMux();
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./app"))));
+	mux.HandleFunc("/healthz", readinessHandler);
+	
 	server:= &http.Server{
 		Addr: ":8080",
 		Handler: mux,
