77be9c8da78c1c0225575afd332e6cca9d6a6605
Krish Koria <krishkoria2004@gmail.com>
2025-03-01 04:39:16 +0000

    Update metrics handler to return HTML response and change API endpoints for metrics and reset

--- main.go
+++ main.go
@@ -26,9 +26,15 @@ func readinessHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+    w.Header().Set("Content-Type", "text/html")
     w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+    fmt.Fprintf(w,`
+        <html>
+          <body>
+            <h1>Welcome, Chirpy Admin</h1>
+            <p>Chirpy has been visited %d times!</p>
+          </body>
+        </html>`, cfg.fileserverHits.Load())
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
@@ -42,12 +48,12 @@ func main()  {
 	mux := http.NewServeMux();
 	cfg := &apiConfig{}
 	mux.Handle("/app/", http.StripPrefix("/app", cfg.middlewareMetricsInc(http.FileServer(http.Dir("./app")))));
-	mux.HandleFunc("GET /healthz", readinessHandler);
-	mux.HandleFunc("GET /metrics", cfg.metricsHandler);
-	mux.HandleFunc("POST /reset", cfg.resetHandler);
+	mux.HandleFunc("GET /api/healthz", readinessHandler);
+	mux.HandleFunc("GET /admin/metrics", cfg.metricsHandler);
+	mux.HandleFunc("POST /admin/reset", cfg.resetHandler);
 	server:= &http.Server{
 		Addr: ":8080",
-		Handler: mux,
+		Handler: mux,	
 	}
 	server.ListenAndServe();
 }
